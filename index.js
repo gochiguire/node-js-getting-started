92c8fdce8dd3549162c4f30c91188093164ce2bd
Angel Aldana <angel.aldana@xappia.com>
2019-05-29 20:23:59 -0300

    Testing arduino route

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => { 
+    res.render('pages/index') 
+  })
+  .get('/arduino', (req, res) => { 
+    res.send({ mensaje: 'hola greyder' }) 
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
